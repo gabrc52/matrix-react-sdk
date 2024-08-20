9b280a8a90590b10745fe6138e6042af797bdbd9
Gabriel Rodríguez <rgabriel@mit.edu>
2024-08-19 21:55:39 -0600

    Show zephyr signatures :)

--- src/components/views/messages/SenderProfile.tsx
+++ src/components/views/messages/SenderProfile.tsx
@@ -27,11 +27,36 @@ interface IProps {
     withTooltip?: boolean;
 }
 
-export default function SenderProfile({ mxEvent, onClick, withTooltip }: IProps): JSX.Element {
+export default function SenderProfile({ mxEvent, onClick, withTooltip }: IProps): JSX.Element {    
     const member = useRoomMemberProfile({
         userId: mxEvent.getSender(),
         member: mxEvent.sender,
     });
+
+    // Zephyr specific stuff!
+
+    let overrideName: string | undefined;
+    let overrideDisambiguation: string | undefined;
+
+    const signature = mxEvent.getContent()['im.zephyr.signature'];
+    // const isAuthentic = mxEvent.getContent()['im.zephyr.authentic'];
+    if (signature) {
+        // This is a Zephyr message
+        // Not extracting it to a variable to avoid merge conflicts in the future
+        const senderId = mxEvent.getSender();
+        if (senderId) {
+            // Example: "@_zephyr_rgabriel:matrix.mit.edu"
+            const kerb = senderId.split(":")[0].substring(9);
+            
+            // This doesn't look that good, commenting out for now...
+            // if (isAuthentic === true) {
+                // overrideName = kerb + " ✅";
+            // } else {
+            overrideName = kerb;
+            // }            
+            overrideDisambiguation = signature;
+        }
+    }
 
     return mxEvent.getContent().msgtype !== MsgType.Emote ? (
         <DisambiguatedProfile
@@ -41,6 +66,8 @@ export default function SenderProfile({ mxEvent, onClick, withTooltip }: IProps)
             colored={true}
             emphasizeDisplayName={true}
             withTooltip={withTooltip}
+            overrideName={overrideName}
+            overrideDisambiguation={overrideDisambiguation}
         />
     ) : (
         <></>
